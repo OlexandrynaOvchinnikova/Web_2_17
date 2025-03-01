965ff926133a41669ebc47442175dfbf034b5481
s1n1s_01 <alexandrinaovchinnikova@gmail.com>
2025-03-01 14:06:12 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
+// src/app/app.component.ts
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+
+
+// const routes: Routes = [
+//   { path: '', component: HomeComponent },
+//   // Додаткові маршрути
+// ];
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet]
 })
 export class AppComponent {
   title = 'cv-app';
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Для Angular
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
